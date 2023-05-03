c7850dde974439b5db9625b225123976cca3c418
João Gabriel Lembo <joaogabriellembo@usp.br>
2023-05-03 18:36:34 -0300

    Inicia contrato para o jogo

---
+++ contracts/MyEpicGame.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract MyEpicGame {
+  constructor() {
+    //nao pode usar acentos nos arquivos .sol, pois dah ruim!
+    console.log("Meu primeiro jogo em crypto! - Ass: JG10");
+  }
+}
